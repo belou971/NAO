e21a274bda5cafacc57f20b8dfa568fb757c4ece
belou971 <belou971@users.noreply.github.com>
2018-05-22 10:57:38 +0200

    Group observations markers as clusters refactor code

--- naoProject/src/TS/NaoBundle/Resources/public/jquery/nao.js
+++ naoProject/src/TS/NaoBundle/Resources/public/jquery/nao.js
@@ -33,8 +33,8 @@ document.getElementById("input-specimen").addEventListener("awesomplete-selectco
 
 $specimen_search_btn = $('#specimen button[name=submit_btn]');
 $specimen_search_btn.on('click', function () {
-    var oldMarkers = getAllMarkers();
-    removeAllMarkers(oldMarkers);
+    //var oldMarkers = getAllMarkers();
+    removeLayers(/*oldMarkers*/);
 
     var $name = $('#input-specimen').val();
     var $data = {specimen_name: $name};
@@ -53,8 +53,7 @@ $specimen_search_btn.on('click', function () {
             if(errors.length === 0) {
                 setResearchMessage(outputMessages,hasFound);
                 if(hasFound) {
-                    var group = outputData.map(addObservationOnMap);
-                    mymap.fitBounds(L.featureGroup(group).getBounds());
+                    AddMarkersToClusterGroup(outputData);
                 }
             }
             else {
@@ -68,29 +67,18 @@ $specimen_search_btn.on('click', function () {
     });
 });
 
-function removeAllMarkers(markers) {
-    $.each(markers, function (idx, marker) {
-        mymap.removeLayer(marker);
-    });
-}
-
-function removeMarker(marker) {
-    mymap.removeLayer(marker);
-}
-
-function getAllMarkers() {
-    var layers = mymap._layers;
-    var markers = [];
-
-    $.each(layers, function (item) {
-        if (layers[item]._latlng) {
-
-            markers.push(this);
-        }
+function removeLayers() {
+    $.each(markersOnMap, function (idx, marker) {
+        clusterGroup.removeLayer(marker);
     });
 
-    return markers;
+    $.each(contourOnMap, function (idx, contour) {
+        mymap.removeLayer(contour);
+    });
+
+    resetMap();
 }
+
 
 /**********************************************************************************************************************/
 /*                                                 Cities auto-completion manager                                     */
@@ -123,6 +111,8 @@ document.getElementById("input-cities").addEventListener("awesomplete-selectcomp
 });
 
 $('#cities button[name=submit_btn]').on('click', function() {
+    removeLayers();
+
     var selected_city = extract_selected_city($('#input-cities').val());
     if(2 === Object.keys(selected_city).length) {
         var url = "https://geo.api.gouv.fr/communes?nom=" + selected_city["nom"] + "&codeDepartement=" + selected_city["departement"] + "&fields=nom&format=geojson&geometry=contour";
@@ -132,37 +122,41 @@ $('#cities button[name=submit_btn]').on('click', function() {
                     var firstFeature = cityInfo["features"][0];
                     cityInfo["features"] = [firstFeature];
                 }
+
+                var data = {city: selected_city["nom"], geo_properties: cityInfo};
+                $.ajax({
+                    type: 'post',
+                    url: Routing.generate('ts_nao_search_specimen_by_city'),
+                    data: JSON.stringify(data),
+                    dataType: 'json',
+                    contentType: "application/json; charset=utf-8",
+                    success: function (response) {
+                        var outputData = response.data;
+                        var outputMessages = response.messages;
+                        var errors = response.errors;
+                        var hasFound = outputData.length > 0;
+
+                        if (errors.length === 0) {
+                            setResearchMessage(outputMessages, hasFound);
+                            //markersOnMap = outputData.map(addObservationOnMap);
+                            AddMarkersToClusterGroup(outputData);
+                        }
+                        else {
+                            setResearchMessage(errors, hasFound);
+                        }
+
+                        var poly = addPolygonOfCity(cityInfo);
+                        contourOnMap.push(poly);
+
+                        var group = poly.getBounds();
+                        mymap.fitBounds(group);
+
+                        displayResearchMeassage();
+
+                        $('#input-cities').val("");
+                    }
+                })
             });
-
-        var data = {city: selected_city["nom"], geo_properties: cityInfo};
-        $.ajax({
-            type: 'post',
-            url: Routing.generate('ts_nao_search_specimen_by_city'),
-            data: JSON.stringify(data),
-            dataType: 'json',
-            contentType: "application/json; charset=utf-8",
-            success: function (response) {
-                var outputData = response.data;
-                var outputMessages = response.messages;
-                var errors = response.errors;
-                var hasFound = outputData.length > 0;
-
-                if (errors.length === 0) {
-                    setResearchMessage(outputMessages, hasFound);
-                    outputData.map(addObservationOnMap);
-                }
-                else {
-                    setResearchMessage(errors, hasFound);
-                }
-
-                var group = addPolygonOfCity(cityInfo).getBounds();
-                mymap.fitBounds(group);
-
-                displayResearchMeassage();
-
-                $('#input-cities').val("");
-            }
-        })
     }
 });
 
@@ -229,7 +223,8 @@ $('#longitude').on("input", enablesearchBtn);
 $('#coord button[name=submit_btn]').on('click', function(event) {
     var coordLat = $('#latitude').val();
     var coordLgn = $('#longitude').val();
-    //var url = "https://geo.api.gouv.fr/communes?fields=code,nom,centre,contour&lat="+coordLat+"&lon="+coordLgn;
+    removeLayers();
+
     var url = "https://geo.api.gouv.fr/communes?lat="+coordLat+"&lon="+coordLgn+"&fields=,nom,code&format=geojson&geometry=contour";
     $.get(url)
         .done (function(coordInfo){
@@ -249,13 +244,17 @@ $('#coord button[name=submit_btn]').on('click', function(event) {
 
                     if (errors.length === 0) {
                         setResearchMessage(outputMessages, hasFound);
-                        outputData.map(addObservationOnMap);
+                        //markersOnMap = outputData.map(addObservationOnMap);
+                        AddMarkersToClusterGroup(outputData);
                     }
                     else {
                         setResearchMessage(errors, hasFound);
                     }
 
-                    var group = addPolygonOfCity(coordInfo).getBounds();
+                    var poly = addPolygonOfCity(coordInfo);
+                    contourOnMap.push(poly);
+
+                    var group = poly.getBounds();
                     mymap.fitBounds(group);
 
                     displayResearchMeassage();
@@ -272,71 +271,39 @@ $('#coord button[name=submit_btn]').on('click', function(event) {
 /**********************************************************************************************************************/
 /*                                                 OpenStreetMap manager                                     */
 /**********************************************************************************************************************/
-var mymap = L.map('mapid').setView([47.102732, 2.443096], 11);
+var mymap = L.map('mapid').setView([46.90296, 1.90925], 6);
 
-var street = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+var mapLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
+    maxZoom: 14,
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoiYmVsb3U5NzEiLCJhIjoiY2pnbXBrMW52MnhzNDJxbnpnb3p5YjU0MCJ9.yh0DNDJQUqv1IHeb9OEbtA'
-}).addTo(mymap);
+});
 
-//var marker = L.marker([48.757766, 2.3231051]).addTo(mymap);
+mymap.addLayer(mapLayer);
 
-/*var circle = L.circle([48.757766, 2.3231051], {
-    color: 'brown',
-    fillColor: '#b73e0b',
-    fillOpacity: 0.8,
-    radius: 90
-}).addTo(mymap);*/
+var markersOnMap = [];
+var contourOnMap = [];
+var clusterGroup = new L.MarkerClusterGroup();
+mymap.addLayer(clusterGroup);
+
+function resetMap() {
+    mymap.setView([46.90296, 1.90925], 6);
+}
+
+
+function AddMarkersToClusterGroup(obsList) {
+    markersOnMap = obsList.map(addObservationOnMap);
+    for (var i = 0; i < markersOnMap.length; i++) {
+        clusterGroup.addLayer(markersOnMap[i]);
+    }
+}
 
 function addObservationOnMap(observation) {
-    return L.marker([observation.latitude, observation.longitude]).addTo(mymap);
+    return L.marker([observation.latitude, observation.longitude], observation.specimen);
 }
 
 function addPolygonOfCity(contour) {
     return L.geoJSON(contour).addTo(mymap);
 }
 
-/*var fresnes = L.circle([48.757766, 2.3231051], {
-    color: 'brown',
-    fillColor: '#b73e0b',
-    fillOpacity: 0.8,
-    radius: 190
-}).bindPopup("<b><u>Fresnes</u></b><br>I am here.");
-
-var paris = L.circle([48.864716, 2.349014], {
-    color: 'brown',
-    fillColor: '#b73e0b',
-    fillOpacity: 0.8,
-    radius: 190
-}).bindPopup("<b><u>Paris</u></b><br>Paris est magique!");
-
-var massy = L.circle([48.730755, 2.271370], {
-    color: 'brown',
-    fillColor: '#b73e0b',
-    fillOpacity: 0.8,
-    radius: 190
-}).bindPopup("<b><u>Massy</u></b><br>Paris - sud");
-
-var lyon = L.circle([45.763584, 4.841425], {
-    color: 'blue',
-    fillColor: '#453e94',
-    fillOpacity: 0.8,
-    radius: 190
-}).bindPopup("<b><u>Lyon</u></b><br>Province");
-
-var ileFrCities = L.layerGroup([fresnes, paris, massy]).addTo(mymap);
-var provCities = L.layerGroup([lyon]).addTo(mymap);
-
-
-var overlayMaps = {
-    "Villes d'ile-de-france": ileFrCities,
-    "Province": provCities
-};
-
-L.control.layers(null, overlayMaps).addTo(mymap);
-
-var group = L.featureGroup([fresnes, paris, massy, lyon]);
-
-mymap.fitBounds(group.getBounds());*/
